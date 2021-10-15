9edd49b60984692935c928170abe58763a8acd1c
JAM-86 <greatscotsinc@gmail.com>
2021-10-15 17:44:41 -0400

    comments added to App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
+//Array of static expense data i.e. 4 expense items
 const DUMMY_EXPENSES = [
   {
     id: "e1",
@@ -10,7 +11,12 @@ const DUMMY_EXPENSES = [
     amount: 94.12,
     date: new Date(2020, 7, 14),
   },
-  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e2",
+    title: "New TV",
+    amount: 799.49,
+    date: new Date(2021, 2, 12),
+  },
   {
     id: "e3",
     title: "Car Insurance",
@@ -25,16 +31,21 @@ const DUMMY_EXPENSES = [
   },
 ];
 
+//App function with 0 parameters used to render entire application to root in index.js with default data at start
 const App = () => {
+  //Array destructuring assigning DUMMY_EXPENSES data into 'expenses' while creating a function 'setExpenses' to allow us to save the state of expense items
   const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
+  //Event function used for setting new expense 'add' is defined as an action i.e. adding new expense, 'Handler' is defined as a event i.e. button click
   const addExpenseHandler = (expense) => {
+    //Set new expense state and save previous expense state. i.e. 'expense' => new state :: 'preExpenses' => previous state. Previous state is saved by utlizing the spread '...' operator
     setExpenses((preExpenses) => {
       return [expense, ...preExpenses];
     });
   };
   return (
     <div>
+      {/*New Expense component used for rendering form i.e. title, amount, dates and buttons. Contains one argument storing 'addExpenseHandler' funciton which is set to 'onAddExpense' prop*/}
       <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
