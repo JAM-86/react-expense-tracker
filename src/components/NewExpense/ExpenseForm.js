54ac8034a88b1a4c98d1d1a40f75c3dfd8570cb2
JAM-86 <greatscotsinc@gmail.com>
2021-10-15 13:15:33 -0400

    Added Chart & ChartBar component

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -57,7 +57,7 @@ const ExpenseForm = (props) => {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
 
